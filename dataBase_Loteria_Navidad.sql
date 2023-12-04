c14c5279e8fa245563a9d56cfa2a766d84409f1c
mrubalv1004 <mrubalv1004@g.educaand.es>
2023-12-04 12:57:36 +0100

    Base de datos en MariaDB

--- dataBase_Loteria_Navidad.sql
+++ dataBase_Loteria_Navidad.sql
@@ -1,26 +1,27 @@
 -- Crear la base de datos
-CREATE DATABASE IF NOT EXISTS loteria_navidad
-USE loteria_navidad
+CREATE DATABASE IF NOT EXISTS loteria_navidad;
+USE loteria_navidad;
 
 -- Crear la tabla de Usuarios
-CREATE TABLE OR REPLACE Usuarios (
+CREATE TABLE IF NOT EXISTS Usuarios (
     usuario VARCHAR(255) PRIMARY KEY,
     pass VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL,
     nombre VARCHAR(255) NOT NULL
 );
 ALTER TABLE Usuarios
-ADD CONSTRAINT chk_role (role IN ('admin', 'user'));
+ADD CONSTRAINT chk_role CHECK (role IN ('admin', 'user'));
 
 -- Crear la tabla de Decimos
-CREATE OR REPLACE TABLE Decimos (
+CREATE TABLE IF NOT EXISTS Decimos (
     numero INT,
+    grupo VARCHAR(60),
     cantidad INT NOT NULL,
     fraccion VARCHAR(10) NOT NULL,
     serie VARCHAR(50) NOT NULL,
     usuario VARCHAR(255),
-    FOREIGN KEY (usuario) REFERENCES Usuarios(usuario) on delete cascade
+    FOREIGN KEY (usuario) REFERENCES Usuarios(usuario) ON DELETE CASCADE
 );
 
 ALTER TABLE Decimos
-add constraint dec_primaryk_pk primary key (numero, usuario);
+ADD CONSTRAINT dec_primaryk_pk PRIMARY KEY (numero, usuario);
