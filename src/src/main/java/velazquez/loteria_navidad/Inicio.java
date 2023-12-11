9b25f92f25ba05d842b896a091929c46e25ec14b
chemaduran <prof.jduran@iesalixar.org>
2023-12-11 11:03:58 +0100

    Creación del proyecto y peticiones async de números

---
+++ src/src/main/java/velazquez/loteria_navidad/Inicio.java
@@ -0,0 +1,24 @@
+package velazquez.loteria_navidad;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet(name = "Inicio")
+public class Inicio extends HttpServlet {
+  static final Logger logger = LoggerFactory.getLogger(Inicio.class);
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    response.setContentType("text/html");
+
+    logger.info("doGet");
+
+    request.getRequestDispatcher("WEB-INF/view/inicio.jsp").forward(request, response);
+  }
+}
---
+++ src/src/main/webapp/WEB-INF/view/inicio.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="js/app.js"></script>
+    <title>Loteria Navidad</title>
+</head>
+<body>
+<h1>Resultados de la loteria de navidad
+</h1>
+
+<div id="results"></div>
+
+</body>
+</html>
