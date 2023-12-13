1b7870bbe8a28f4e313b0fa01eb37258d125824e
Manuel Cruz <mcrufor194@g.educaand.es>
2023-12-13 09:13:39 +0100

    Triggers para controlar que no se compran más de 20 décimos del mismo número

--- documentacion/triggers.sql
+++ documentacion/triggers.sql
@@ -24,17 +24,15 @@ CREATE OR REPLACE TRIGGER loteria_navidad.tr_insert_solo_20_decimos
 BEGIN
     DECLARE v_cantidad INT;
 
-    SELECT SUM(cantidad) + NEW.cantidad
+    SELECT NVL(SUM(cantidad), 0) + NEW.cantidad
     INTO v_cantidad
     FROM Decimos
     WHERE numero = NEW.numero;
 
     IF v_cantidad > 20 THEN
+
         SIGNAL SQLSTATE '45000'
         SET MYSQL_ERRNO=30001,
             MESSAGE_TEXT='No puede comprarse más de 20 décimos del mismo número';
     END IF;
 END;
-
-INSERT INTO loteria_navidad.Usuarios VALUES ('Johnny', 'Johnny', 'user', 'Johnny');
-INSERT INTO loteria_navidad.Decimos VALUES (1, 'Wololo', 21, 1, 1, 'Johnny');
