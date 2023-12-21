33dcfae99dab4705942295af19874b9d44228282
josgonmor <josan4452@gmail.com>
2023-12-21 08:49:00 +0100

    html administrador funcionando

--- src/src/main/java/velazquez/loteria_navidad/controllers/Inicio.java
+++ src/src/main/java/velazquez/loteria_navidad/controllers/Inicio.java
@@ -24,7 +24,7 @@ public class Inicio extends HttpServlet {
         logger.info("doGet");
 
 //        request.getRequestDispatcher("WEB-INF/view/inicio.jsp").forward(request, response);
-        request.getRequestDispatcher("WEB-INF/view/private/administrador.html").forward(request, response);
+        request.getRequestDispatcher("WEB-INF/view/private/administrador.jsp").forward(request, response);
 //    request.getRequestDispatcher("WEB-INF/test/test.jsp").forward(request, response);
     }
 
